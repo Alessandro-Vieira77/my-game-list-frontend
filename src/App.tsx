fc94cae395ef5edd6b08714f24ed3ab394e72f85
Alessandro-Vieira77 <alessandrovieiraa77@gmail.com>
2025-10-16 11:02:39 -0300

    feat: add design system shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from './components/ui/button'
+
 function App() {
   return (
     <div>
       <h1 className="m-2 text-3xl font-bold text-red-500">Hello World!</h1>
+      <Button className="m-3">Click me</Button>
     </div>
   )
 }
